807b22364e2c6c858d5496a4732177f7421725b5
Tyler Holston <tylerholston@1726nerds.org>
2019-01-08 17:46:56 -0700

    Started creating config loader and globals

---
+++ src/main/include/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+/* constexpr std::string DRIVE_JOY_MAX = "Joystick/Drivetrain/Max Speed"
+constexpr std::string DRIVE_JOY_MIN = "Joystick/Drivetrain/Min Speed"
+constexpr std::string DRIVE_JOY_DEADZONE = "Joystick/Drivetrain/Deadzone"
+constexpr std::string 
+*/
+template <constexpr std::string DRIVE_JOY_MAX, "Joystick/Drivetrain/Max Speed">
+template <constexpr std::string DRIVE_JOY_MIN, "Joystick/Drivetrain/Min Speed">
+template <constexpr std::string DRIVE_JOY_DEADZONE, "Joystick/Drivetrain/Deadzone">
