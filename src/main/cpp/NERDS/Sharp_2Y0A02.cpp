7091cb24418966ee6e01ffbc3691612537aeda17
Lauren Wall <laurenwall@1726nerds.org>
2019-01-18 17:52:34 -0700

    Created and started Sharp 2Y0A02

---
+++ src/main/cpp/NERDS/Sharp_2Y0A02.cpp
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "NERDS/Sharp_2Y0A02.h"
+
+#include <frc/Driverstation.h>
+
+Sharp_2Y0A02::Sharp_2Y0A02(int channel) : frc::AnalogInput(channel) {
+
+}
+
+double Sharp_2Y0A02::GetDistance() {
+    double voltage = GetVoltage();
+    if(voltage > 4.8 || voltage < .9){
+        frc::DriverStation::ReportError("Sensor value out of range");
+        return -1;
+    }
+
+    double distance = 9.462/(voltage - 0.01692);
+    return distance;
+}
---
+++ src/main/include/NERDS/Sharp_2Y0A02.h
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/AnalogInput.h>
+
+class Sharp_2Y0A02 : public frc::AnalogInput {
+ public:
+  Sharp_2Y0A02(int);
+  double GetDistance();
+};
