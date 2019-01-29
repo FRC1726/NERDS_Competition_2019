d9389bddd9dc8d0e8a5e75368f8b9cd19729a5a2
Lauren Wall <laurenwall@1726nerds.org>
2019-01-28 16:51:20 -0800

    Started creating subsystem for lifting the robot to the platforms

---
+++ src/main/include/subsystems/Lift.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+
+#include <frc/DigitalInput.h>
+#include <frc/VictorSP.h>
+
+class Lift : public frc::Subsystem {
+public:
+  Lift();
+  void InitDefaultCommand() override;
+  void run(double);
+private:
+  frc::DigitalInput reverseLimitSwitch;
+  frc::VictorSP liftMotor;
+};
